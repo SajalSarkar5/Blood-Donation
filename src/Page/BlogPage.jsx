c871b5bfb88ffe62f4490c4ac331f1fdc9a9c7ea
SajalSarkar5 <sarkarsajal810@gmail.com>
2023-11-29 14:23:36 +0600

    added donor page section

--- src/Page/BlogPage.jsx
+++ src/Page/BlogPage.jsx
@@ -1,6 +1,7 @@
 
 
 const BlogPage = () => {
+    
     return (
         <div>
 
@@ -245,7 +246,6 @@ const BlogPage = () => {
                 <iframe className="w-full" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29410.087214845324!2d89.52798669999999!3d22.866818600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1701180822480!5m2!1sen!2sbd" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
             </div>
-
 
         </div>
     );
