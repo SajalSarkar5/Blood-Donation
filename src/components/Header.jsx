bb5ec0c45d7d99ff2540abd90f7f209765d112ec
SajalSarkar5 <sarkarsajal810@gmail.com>
2023-11-29 14:17:12 +0600

    added Featured Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,10 +61,7 @@ const Header = () => {
                                 !isAdmin && !isDoner && <NavLink to="/volunteer" class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" >Dashboard</NavLink>
                             }
 
-
-
                             <NavLink to="/blogpage" class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" >Blog Page</NavLink>
-
 
                             {
                                 user ?
