47401e2b460b028db7056936e31d01be20bcd316
SajalSarkar5 <sarkarsajal810@gmail.com>
2023-11-29 14:24:46 +0600

    added admin page

--- src/Page/Footer.jsx
+++ src/Page/Footer.jsx
@@ -4,8 +4,6 @@ const Footer = () => {
     return (
         <div>
 
-
-
             <footer class="bg-white rounded-lg shadow dark:bg-gray-900">
                 <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div class="sm:flex sm:items-center sm:justify-between">
